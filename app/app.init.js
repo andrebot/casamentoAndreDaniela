c494028f9ca7b48705047231af3c4832d827ab9c
Andre Botelho <andrebot_almeida@hotmail.com>
2017-02-01 00:17:27 -0200

    Fixing ie issue

--- app/app.init.js
+++ app/app.init.js
@@ -12,7 +12,10 @@
     var apiKey = 'AIzaSyB_dMEBT5lOT8BQlaPA5HRez0ZMWQln_3s';
     var databaseURL = 'https://weddingsite-a7b7e.firebaseio.com';
 
-    firebase.initializeApp({apiKey,databaseURL});
+    firebase.initializeApp({
+      apiKey: apiKey,
+      databaseURL: databaseURL
+    });
 
     $urlRouterProvider.otherwise('/');
 
--- gulp/tasks/build/build-js.js
+++ gulp/tasks/build/build-js.js
@@ -6,10 +6,10 @@ const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const gulp = require('gulp');
 
-gulp.task('build:js', function () {
+gulp.task('build:js', function (cb) {
   return gulp.src([config.initPath, config.jsPath])
     .pipe(concat('app.js'))
     .pipe(ngAnnotate())
-    //.pipe(uglify())
+    .pipe(uglify())
     .pipe(gulp.dest(config.distFolder));
 });
